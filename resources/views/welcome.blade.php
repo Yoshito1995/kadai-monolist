949aadf49b555023fd26366f0b9a52e7270ed4c5
taro.kirameki <taro.kirameki@techacademy.jp>
2018-06-22 05:34:05 +0000

    rakuten api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,9 @@
     <div class="cover">
         <div class="cover-inner">
             <div class="cover-contents">
-                <h1>素敵なモノと出会う場所</h1>
+                <h1>なんか文句あんのか</h1>
                 @if (!Auth::check())
-                    <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">モノリストを始める</a>
+                    <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">始められると思うなよ</a>
                 @endif
             </div>
         </div>
